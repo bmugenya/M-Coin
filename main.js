32b1b7e44a7d0ab162e6588154796824c2cacd83
bmugenya26@gmail.com <bmugenya26@gmail.com>
2019-02-03 18:26:51 +0300

    reviewed the code

--- main.js
+++ main.js
@@ -32,6 +32,23 @@ class Blockchain{
         newBlock.hash = newBlock.calculateHash();
         this.chain.push(newBlock);
     } 
+
+    isChainValid(){
+        for(let i = 1; i < this.chain.length; i++){
+            const currentBlock = this.chain[i];
+            const prevHash = this.chain[i -1];
+
+            if(currentBlock.hash != currentBlock.calculateHash()){
+                return false;
+            }
+
+            if(currentBlock.prevHash != prevHash.hash){
+                return fasle;
+            }
+    
+            return true;
+        }
+    }
 }
 
 let medhaPesa = new Blockchain();
@@ -39,4 +56,6 @@ medhaPesa.addBlock(new Block(1,"2019-02-03" ,{ amount : 4}));
 medhaPesa.addBlock(new Block(1,"2019-02-03 ",{ amount : 10}));
 
 
-console.log(JSON.stringify(medhaPesa,null,4));
+//console.log(JSON.stringify(medhaPesa,null,4));
+
+console.log("is blockchain valid?" + medhaPesa.isChainValid())
