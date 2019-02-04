708cf6464a8be504122e5e5b7759e9876e48ab98
bmugenya26@gmail.com <bmugenya26@gmail.com>
2019-02-04 09:29:50 +0300

    [Feature #163688350] implemented  proof of work for each block

--- main.js
+++ main.js
@@ -5,18 +5,29 @@ class Block {
         this.timestamp = timestamp;
         this.data = data;
         this.hash = this.calculateHash();
+        this.nonce = 0;
 
     }
 
     calculateHash(){
 
-        return SHA256(this.index + this.prevHash + this.timestamp + JSON.stringify(this.data)).toString()
+        return SHA256(this.index + this.prevHash + this.timestamp + JSON.stringify(this.data) + this.nonce).toString()
+    }
+
+    mineBlock(difficulty){
+        while(this.hash.substring(0, difficulty) !== Array(difficulty + 1).join("0")){
+            this.nonce++;
+            this.hash = this.calculateHash();
+        }
+        console.log("Block mined: " + this.hash)
     }
 }
+
 
 class Blockchain{
     constructor(){
         this.chain = [this.createGenesisBlock()];
+        this.difficulty = 4;
     }
 
     createGenesisBlock(){
@@ -29,7 +40,7 @@ class Blockchain{
 
     addBlock(newBlock){
         newBlock.prevHash = this.getLatestBlock().hash;
-        newBlock.hash = newBlock.calculateHash();
+        newBlock.mineBlock(this.difficulty);
         this.chain.push(newBlock);
     } 
 
@@ -52,10 +63,20 @@ class Blockchain{
 }
 
 let medhaPesa = new Blockchain();
+console.log("Mining Block 1...")
 medhaPesa.addBlock(new Block(1,"2019-02-03" ,{ amount : 4}));
+console.log("Mining Block 2...")
 medhaPesa.addBlock(new Block(1,"2019-02-03 ",{ amount : 10}));
 
 
-//console.log(JSON.stringify(medhaPesa,null,4));
 
-console.log("is blockchain valid?" + medhaPesa.isChainValid())
+
+
+
+
+
+
+
+
+
+
