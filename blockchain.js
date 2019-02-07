df50d199966fc8f4e74958b51b9c0903acecdd21
bmugenya26@gmail.com <bmugenya26@gmail.com>
2019-02-07 20:53:21 +0300

    [Feature #163795096] added private and public key

--- blockchain.js
+++ blockchain.js
@@ -1,10 +1,37 @@
 const SHA256 = require('crypto-js/sha256');
+const EC = require('elliptic').ec;
+
+const ec = new EC('secp256k1');
 
 class Transaction{
     constructor(fromAddress, toAddress, amount){
         this.fromAddress = fromAddress;
         this.toAddress = toAddress;
         this.amount = amount; 
+    }
+
+    calculateHash(){
+        return SHA256(this.fromAddress + this.toAddress + this.amount).toString();
+    }
+
+    signTransaction(signKey){
+
+        if(signKey.getPublic('hex') !== this.fromAddress){
+            throw new Error('You cannot sign transactions for other wallets!');
+        }
+
+        const hashTx = this.calculateHash();
+        const sig = signKey.sign(hashTx,'base64');
+        this.signature = sig.toDER('hex');
+    }
+
+    isValid(){
+        if(this.fromAddress === null) return true;
+        if(!this.signature || this.signature.length === 0){
+            throw new Error('No signature in this transaction');
+        }
+        const publicKey = ec.keyFromPublic(this.fromAddress, 'hex');
+        return publicKey.verify(this.calculateHash(), this.signature);
     }
 }
 
@@ -29,6 +56,15 @@ class Block {
         }
         console.log("Block mined: " + this.hash)
     }
+
+    hasValidTransactions(){
+        for(const tx of this.transactions){
+            if(!tx.isValid()){
+                return false;
+            }
+        }
+        return true;
+    }
 }
 
 
@@ -37,7 +73,7 @@ class Blockchain{
         this.chain = [this.createGenesisBlock()];
         this.difficulty = 2;
         this.pendingTransactions = [];
-        this.miningReward = 100;
+        this.miningReward = 10;
     }
 
     createGenesisBlock(){
@@ -60,11 +96,19 @@ class Blockchain{
         ];
     }
 
-    createTransactions(transaction){
+    addTransactions(transaction){
+        
+        if(!transaction.fromAddress || !transaction.toAddress){
+            throw new Error('Transaction must include from and to address');
+        }
+
+        if(!transaction.isValid()){
+            throw new Error('Cannot add invalid transaction to chain');
+        }
         this.pendingTransactions.push(transaction)
     }
 
-    getBalancedOfAddress(address){
+    getBalanceOfAddress(address){
         let balance = 0;
 
         for(const block of this.chain){
@@ -84,6 +128,10 @@ class Blockchain{
         for(let i = 1; i < this.chain.length; i++){
             const currentBlock = this.chain[i];
             const prevHash = this.chain[i -1];
+
+            if(!currentBlock.hasValidTransactions()){
+                return false;
+            }
 
             if(currentBlock.hash != currentBlock.calculateHash()){
                 return false;
--- main.js
+++ main.js
@@ -1,19 +1,23 @@
 const { Blockchain, Transaction} = require('./blockchain');
+const EC = require('elliptic').ec;
+const ec = new EC('secp256k1');
+
+const myKey = ec.keyFromPrivate('22e9763a26234f2120f6cb97ac991fd111cd1b914a60c1306e643a21dee49e3f');
+const myWalletAddress = myKey.getPublic('hex');
+
+
 let mCoin = new Blockchain();
-mCoin.createTransactions(new Transaction('address1','address2',100));
-mCoin.createTransactions(new Transaction('address1','address2',50));
+
+const tx1 = new Transaction(myWalletAddress,'public key goes here',10);
+tx1.signTransaction(myKey);
+mCoin.addTransactions(tx1)
+
+
 
 console.log("\n Starting the minner....");
-mCoin.minePendingTransactions("Mugenya-address");
+mCoin.minePendingTransactions(myWalletAddress);
 
-console.log('\nBalance of Mugenya is', mCoin.getBalancedOfAddress('Mugenya-address'));
-
-
-console.log("\n Starting the minner again....");
-mCoin.minePendingTransactions("Mugenya-address");
-
-console.log('\nBalance of Mugenya is', mCoin.getBalancedOfAddress('Mugenya-address'));
-
+console.log('\nBalance of Mugenya is', mCoin.getBalanceOfAddress(myWalletAddress));
 
 
 
